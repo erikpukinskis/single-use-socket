5c0d34a15c8e8400e3dc49fec01bff4c129df5b1
Erik Pukinskis <erik@snowedin.net>
2015-10-14 08:32:43 -0700

    Fix async in test

--- single-use-socket.js
+++ single-use-socket.js
@@ -3,8 +3,8 @@ var library = require("nrtv-library")(require)
 module.exports = library.export(
   "nrtv-single-use-socket",
 
-  ["nrtv-socket-server", "querystring", "nrtv-server"],
-  function(socketServer, querystring, nrtvServer) {
+  ["nrtv-socket-server", "querystring", "nrtv-server", "nrtv-browser-bridge"],
+  function(socketServer, querystring, nrtvServer, bridge) {
 
     var sockets = {}
 
@@ -23,6 +23,32 @@ module.exports = library.export(
         this.handler = handler
       }
 
+    SingleUseSocket.prototype.send =
+      function(message) {
+        this.connection.write(message)
+      }
+
+    SingleUseSocket.prototype.defineListenInBrowser =
+      function() {
+        var binding = bridge.defineFunction(
+          [socketServer.defineInBrowser()],
+          function(getSocket, id, callback) {
+
+            getSocket(
+              listen,
+              "?__nrtvSingleUseSocketIdentifier="+id
+            )
+
+            function listen(socket) {
+              socket.onmessage = callback
+            }
+
+          }
+        )
+
+        return binding.withArgs(this.identifer)  
+      }
+
     SingleUseSocket.getReady =
       function() {
         if (!alreadyListening) {
@@ -37,16 +63,25 @@ module.exports = library.export(
 
       var params = querystring.parse(connection.url.split("?")[1])
 
+      var socket = this
+
       if (id = params.__nrtvSingleUseSocketIdentifier) {
 
         var socket = sockets[id]
 
         if (socket) {
+          socket.connection = connection
           socket.onReady()
-          connection.on("data", socket.handler)
+          connection.on("data", function() {
+              socket.handler.apply(null, arguments)
+            }
+          )
           connection.on("close",
             function() {
               delete sockets[id]
+              if (socket.onClose) {
+                socket.onClose()
+              }
             }
           )
           return
@@ -55,6 +90,11 @@ module.exports = library.export(
 
       next()
     }
+
+    SingleUseSocket.prototype.onClose =
+      function(callback) {
+        this.onClose = callback
+      }
 
     SingleUseSocket.prototype.url =
       function() {
--- test.js
+++ test.js
@@ -1,9 +1,45 @@
 var test = require("nrtv-test")(require)
+var library = test.library
+
+test.using(
+  "defines a listen function in the browser",
+  ["./single-use-socket", "nrtv-browse", "nrtv-browser-bridge", library.reset("nrtv-server")],
+  function(expect, done, SingleUseSocket, browse, bridge, server) {
+
+    var socket = new SingleUseSocket(
+      function() {
+        socket.send("hi!")
+
+        setTimeout(function() {
+          browser.assert.text("body", "all your representative are belong to us")
+          done()
+          server.stop()
+        }, 50)
+      }
+    )
+
+    var win = bridge.defineFunction(
+      function gerrymander(message) {
+        document.querySelector("body").innerHTML = "all your representative are belong to us"
+      }
+    )
+
+    var listen = socket.defineListenInBrowser().withArgs(win)
+
+    bridge.asap(listen)
+
+    server.get("/", bridge.sendPage())
+
+    server.start(9913)
+
+    var browser = browse("http://localhost:9913")
+  }
+)
 
 test.using(
   "receives a message and then stops working (as we would hope)",
-  ["./single-use-socket", "nrtv-server", "ws", "nrtv-socket-server", "sinon"],
-  function(expect, done, SingleUseSocket, server, WebSocket, socketServer, sinon) {
+  ["./single-use-socket", library.reset("nrtv-server"), "ws", "nrtv-socket-server", "sinon", "nrtv-browse", "nrtv-browser-bridge"],
+  function(expect, done, SingleUseSocket, server, WebSocket, socketServer, sinon, browse, bridge) {
 
     var fallback = sinon.spy()
 
@@ -24,40 +60,35 @@ test.using(
 
     socket.listen(function(message) {
       expect(isConnected).to.be.true
-      
+
       expect(message).to.equal("burfday chex")
+
+      expect(fallback).not.to.have.been.called
 
       ws.close()
     })
+
+    socket.onClose(function() {
+      var other = new WebSocket(socket.url())
+
+      other.on("open", function() {
+        other.send("brambo", expectFallback)
+      })
+    })
+
+    function expectFallback() {
+      expect(fallback).to.have.been.called
+      done()
+      server.stop()
+    }
 
     server.start(1187)
 
     ws = new WebSocket(socket.url())
 
     ws.on("open", function() {
-      ws.send("burfday chex", hopeWeUsedTheSUS)
+      ws.send("burfday chex")
     })
-
-    function hopeWeUsedTheSUS() {
-
-      expect(fallback).not.to.have.been.called
-
-      ws.close()
-
-      setTimeout(function() {
-        var other = new WebSocket(socket.url())
-
-        other.on("open", function() {
-          other.send("brambo", expectFallbackNow)
-        })
-      })
-    }
-
-    function expectFallbackNow() {
-      expect(fallback).to.have.been.called
-      done()
-      server.stop()
-    }
 
   }
 )
